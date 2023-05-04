93e1a2a82f6b8f01782a7482dfa608ed4a1c6535
Moh Dev <73759551+MohamedCS1@users.noreply.github.com>
2023-05-04 22:08:12 +0100

    create card movie layout

--- data/src/main/java/com/example/data/repository/TrendingMoviesRemoteDataSourceImpl.kt
+++ data/src/main/java/com/example/data/repository/TrendingMoviesRemoteDataSourceImpl.kt
@@ -3,7 +3,6 @@ package com.example.data.repository
 import com.example.data.remote.ApiService
 import com.example.domain.models.trendingMovies.TrendingMoviesResponse
 import com.example.domain.repository.TrendingMoviesRemoteDataSource
-import kotlinx.coroutines.withContext
 
 class TrendingMoviesRemoteDataSourceImpl(private val apiService: ApiService ,private val apiKey:String):TrendingMoviesRemoteDataSource {
     override fun getTrendingMoviesFromRemote(apiKey: String): TrendingMoviesResponse = apiService.getTrendingMovies(apiKey)
